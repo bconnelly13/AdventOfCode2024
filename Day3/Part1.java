da21ffe34efc69603db592a7561bf5061e3eb977
Ben Connelly <143213546+bconnelly13@users.noreply.github.com>
2024-12-03 14:32:08 -0500

    Day 3 Setup

---
+++ Day3/Part1.java
@@ -0,0 +1,13 @@
+package Day3;
+
+public class Part1 {
+    
+    static String filename = "input.txt";
+    //static String filename = "testinput.txt";     // answer = 161
+
+    public static void main(String[] args) {
+        
+    }
+
+
+}
---
+++ Day3/Part2.java
@@ -0,0 +1,12 @@
+package Day3;
+
+public class Part2 {
+
+    static String filename = "input.txt";
+    // static String filename = "testinput.txt"; // answer =
+
+    public static void main(String[] args) {
+
+    }
+
+}
