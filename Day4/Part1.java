a12f22c8bb51bf543c1ed1d3c8118cb511277961
Ben Connelly <143213546+bconnelly13@users.noreply.github.com>
2024-12-04 09:45:12 -0500

    Day 4 Setup

---
+++ Day4/Part1.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.nio.Buffer;
+
+public class Part1 {
+    
+    // static String filename = "Day4/input.txt";
+    static String filename = "Day4/testinput.txt";
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(filename));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Day4/Part2.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Part2 {
+    // static String filename = "Day4/input.txt";
+    static String filename = "Day4/testinput.txt";
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(filename));
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
