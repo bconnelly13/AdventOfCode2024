e066dee62fbf294f12fa0f35a71e77a178b53075
Ben Connelly <143213546+bconnelly13@users.noreply.github.com>
2024-12-16 11:36:45 -0500

    Day 6 Setup

---
+++ Day6/Part2.java
@@ -0,0 +1,30 @@
+package Day6;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class Part2 {
+
+    static String filename = "Day6/testinput.txt";
+    // static String filename = "Day6/input.txt";
+
+    public static void main(String[] args) {
+        try {
+            BufferedReader bf = new BufferedReader(new FileReader(filename));
+            int answer = 0;
+
+
+
+
+
+
+
+
+
+            System.out.printf("Part 2 Answer: %d\n", answer);
+        } catch (Exception e) {
+            System.out.println("try catch error");
+            e.printStackTrace();
+        }
+    }
+}
