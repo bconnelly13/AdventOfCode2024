abed4a7481037821fe85f4510dc2f9d974c4276d
Ben Connelly <143213546+bconnelly13@users.noreply.github.com>
2024-12-16 16:08:03 -0500

    Day 7 Part 1

--- Day7/Part1.java
+++ Day7/Part1.java
@@ -2,16 +2,43 @@ package Day7;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.util.Arrays;
 
 public class Part1 {
-    static String filename = "Day6/testinput.txt";
-    // static String filename = "Day6/input.txt";
+    // static String filename = "Day7/testinput.txt";
+    static String filename = "Day7/input.txt";
 
     public static void main(String[] args) {
         try {
             BufferedReader bf = new BufferedReader(new FileReader(filename));
-            int answer = 0;
+            long answer = 0;
 
+            String line = bf.readLine();
+            while (line != null) {
+                int colonIndex = line.indexOf(":");
+                long target = Long.parseLong(line.substring(0, colonIndex));
+                String[] numStrs = line.substring(colonIndex + 2).split(" ");
+                int len = numStrs.length;
+                long[] nums = new long[len];
+                for (int i = 0; i < len; i++) {
+                    nums[i] = Long.parseLong(numStrs[i]);
+                }
+
+                long[] results = mathOptions(nums);
+                // for (int i = 0; i < results.length; i++) {
+                //     System.out.printf("%d ", results[i]);
+                // }
+                System.out.println();
+                for (int i = 0; i < results.length; i++) {
+                    if (results[i] == target) {
+                        answer += target;
+                        break;
+                    }
+                }
+
+
+                line = bf.readLine();
+            }
 
 
 
@@ -21,4 +48,23 @@ public class Part1 {
             e.printStackTrace();
         }
     }
+
+    public static long[] mathOptions(long[] nums) {
+        if (nums.length == 1) {
+            return nums;
+        }
+
+        long[] result = mathOptions(Arrays.copyOfRange(nums, 0, nums.length - 1));
+        long[] returnArr = new long[result.length * 2];
+        for (int i = 0; i < result.length; i++) {
+            returnArr[2*i] = nums[nums.length - 1] + result[i];
+            returnArr[2*i+1] = nums[nums.length - 1] * result[i];
+        }
+        return returnArr;
+
+
+    }
+
+
+
 }
