c87de48587db41df1da69275e5559805f4c41126
Ben Connelly <143213546+bconnelly13@users.noreply.github.com>
2024-12-16 17:32:30 -0500

    Day 8 Part 1

--- Day8/Part1.java
+++ Day8/Part1.java
@@ -3,18 +3,104 @@ package Day8;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.nio.Buffer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Part1 {
-    static String filename = "Day8/testinput.txt";
-    // static String filename = "Day8/input.txt";
+    // static String filename = "Day8/testinput.txt";
+    static String filename = "Day8/input.txt";
 
     public static void main(String[] args) {
         try {
             BufferedReader bf = new BufferedReader(new FileReader(filename));
             int answer = 0;
 
+            ArrayList<String[]> listInput = new ArrayList<>();
+            String line = bf.readLine();
+            while (line != null) {
+                listInput.add(line.split(""));
+                line = bf.readLine();
+            }
+
+            int lines = listInput.size();
+            int lineLen = listInput.get(0).length;
+            String[][] map = new String[lines][lineLen];
+            boolean[][] antinodes = new boolean[lines][lineLen];
+            for (int i = 0; i < lines; i++) {
+                map[i] = listInput.get(i);
+                for (int j = 0; j < lineLen; j++) {
+                    antinodes[i][j] = false;
+                }            
+            }
 
 
+            HashMap<String, ArrayList<int[]>> antennas = new HashMap<>();
+            for (int i = 0; i < lines; i++) {
+                for (int j = 0; j < lineLen; j++) {
+                    String c = map[i][j];
+                    if (!c.equals(".")) {
+                        int[] newPair = {i, j};
+                        ArrayList<int[]> pairs;
+                        if (antennas.containsKey(c)) {
+                            pairs = antennas.get(c);
+                        } else {
+                            pairs = new ArrayList<>();
+                        }
+                        pairs.add(newPair);
+                        antennas.put(c, pairs);
+                    }
+                }
+            }
+
+
+            Set<String> keys = antennas.keySet();
+            for (String c : keys) {
+                ArrayList<int[]> pairs = antennas.get(c);
+                int npairs = pairs.size();
+                for (int i = 0; i < npairs; i++) {
+                    int[] pair1 = pairs.get(i);
+                    for (int j = i + 1; j < npairs; j++) {
+                        int[] pair2 = pairs.get(j);
+                        int ydiff = pair1[0] - pair2[0];
+                        int xdiff = pair1[1] - pair2[1];
+
+                        int newY = pair1[0] + ydiff;
+                        int newX = pair1[1] + xdiff;
+
+                        if (newY >= 0 && newY < lines && newX >= 0 && newX < lineLen) {
+                            antinodes[newY][newX] = true;
+                            // if (map[newY][newX].equals(".")) {
+                                // map[newY][newX] = "#";
+                            // }
+                        }
+
+                        newY = pair2[0] - ydiff;
+                        newX = pair2[1] - xdiff;
+
+                        if (newY >= 0 && newY < lines && newX >= 0 && newX < lineLen) {
+                            antinodes[newY][newX] = true;
+                            // if (map[newY][newX].equals(".")) {
+                                // map[newY][newX] = "#";
+                            // }
+                        }
+
+                    }
+                }
+            }
+
+            
+
+            for (int i = 0; i < lines; i++) {
+                for (int j = 0; j < lineLen; j++) {
+                    // System.out.printf("%s", map[i][j]);
+                    if (antinodes[i][j]) {
+                        answer++;
+                    }
+                }
+                // System.out.println();
+            }
 
 
             System.out.printf("Part 1 Answer: %d\n", answer);
